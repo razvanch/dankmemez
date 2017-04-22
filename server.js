a8781aa9c6c6d31b5be57fc99436564b647e00d5
Razvan Chitu <razvan.ch95@gmail.com>
2017-04-22 17:51:43 +0300

    add simple image upload

--- server.js
+++ server.js
@@ -1,34 +1,44 @@
-
+"use strict";
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+const http = require('http');
+const routes = require('./routes');
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+ 
+// default options 
+app.use(fileUpload());
 
-var app = express();
+app.set('port', process.env.PORT || 3000);
+ 
+app.post('/images', function(req, res) {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+ 
+  let sampleFile = req.files.sampleFile;
+ 
+  console.log(sampleFile);
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  res.status(200).send(sampleFile.name);
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.get('/images', function(req, res) {
+  res.send("<html>" +
+              "<body>" +
+                "<form ref='uploadForm' " +
+                  "id='uploadForm' " +
+                  "action='http://localhost:3000/images' " +
+                  "method='post' " +
+                  "encType='multipart/form-data'>" +
+                    "<input type='file' name='sampleFile' />" +
+                    "<input type='submit' value='Submit image!' />" +
+                "</form>" +
+              "</body>" +
+          "</html>");
 });
-
-app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
