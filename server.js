74359636b4cb664be900820a419ddb19c6e59c41
Razvan Chitu <razvan.ch95@gmail.com>
2017-04-22 18:11:30 +0300

    add dummy images implementation

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(fileUpload());
 
 app.set('port', process.env.PORT || 3000);
  
-app.post('/images', function(req, res) {
+app.post('/upload', function(req, res) {
   if (!req.files)
     return res.status(400).send('No files were uploaded.');
  
@@ -25,12 +25,12 @@ app.post('/images', function(req, res) {
   res.status(200).send(sampleFile.name);
 });
 
-app.get('/images', function(req, res) {
+app.get('/upload', function(req, res) {
   res.send("<html>" +
               "<body>" +
                 "<form ref='uploadForm' " +
                   "id='uploadForm' " +
-                  "action='http://icaption.azurewebsites.net/images' " +
+                  "action='http://icaption.azurewebsites.net/upload' " +
                   "method='post' " +
                   "encType='multipart/form-data'>" +
                     "<input type='file' name='sampleFile' />" +
@@ -38,6 +38,33 @@ app.get('/images', function(req, res) {
                 "</form>" +
               "</body>" +
           "</html>");
+});
+
+app.get('/images', function(req, res) {
+  const images = [
+    { url: 'https://memezstorage.blob.core.windows.net/demoblockblobcontainer/picture0',
+      caption: 'Aaron Hernandez eating a hot dog' },
+    { url: 'https://memezstorage.blob.core.windows.net/demoblockblobcontainer/picture1',
+      caption: 'Chester Bennington et al. pose for a picture' },
+    { url: 'https://memezstorage.blob.core.windows.net/demoblockblobcontainer/picture2',
+      caption: 'a little boy holding a baseball bat' },
+    { url: 'https://memezstorage.blob.core.windows.net/demoblockblobcontainer/picture3',
+      caption: 'a woman standing in the dark' },
+    { url: 'https://memezstorage.blob.core.windows.net/demoblockblobcontainer/picture4',
+      caption: 'a bird perched on top of a tree' },
+    { url: 'https://memezstorage.blob.core.windows.net/demoblockblobcontainer/picture5',
+      caption: 'a small dog' },
+    { url: 'https://memezstorage.blob.core.windows.net/demoblockblobcontainer/picture6',
+      caption: 'a blender filled with coffee' },
+    { url: 'https://memezstorage.blob.core.windows.net/demoblockblobcontainer/picture7',
+      caption: 'person sitting on a bed' },
+    { url: 'https://memezstorage.blob.core.windows.net/demoblockblobcontainer/picture8',
+      caption: 'a red and white cake' },
+    { url: 'https://memezstorage.blob.core.windows.net/demoblockblobcontainer/picture9',
+      caption: 'a group of people walking down a rainy city street' }
+  ];
+
+  res.json(images);
 });
 
 http.createServer(app).listen(app.get('port'), function(){
